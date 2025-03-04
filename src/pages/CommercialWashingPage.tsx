d00b1f199d648a644277fa8e4eec9c9402b7fac7
Tony Mputhia <tonymmputhia@gmail.com>
2025-03-04 21:20:22 +0300

    Add commercial washing page

---
+++ src/pages/CommercialWashingPage.tsx
@@ -0,0 +1,10 @@
+const CommercialCleaning = () => {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold">Commercial Cleaning</h1>
+        <p className="mt-4">We provide top-notch commercial washing services.</p>
+      </div>
+    );
+  };
+  
+  export default CommercialCleaning;
