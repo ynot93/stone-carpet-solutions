6ea1796ce236c16ea8a71340873a1eaab7441a52
Tony Mputhia <tonymmputhia@gmail.com>
2025-03-04 21:19:28 +0300

    Route to relevant pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CommercialCleaning from "./pages/CommercialWashingPage";
+import OurStory from "./pages/OurStoryPage";
+import ResidentialCleaning from "./pages/ResidentialCleaningPage";
 import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Router basename="/sam-car-wash">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/residential-cleaning" element={<ResidentialCleaning />} />
+        <Route path="/commercial-washing" element={<CommercialCleaning />} />
+        <Route path="/our-story" element={<OurStory />} />
+      </Routes>
+    </Router>
   )
 }
 
