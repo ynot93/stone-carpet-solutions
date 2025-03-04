f3110051a561f5df4e94b05b624dccc75b580994
Tony Mputhia <tonymmputhia@gmail.com>
2025-03-04 21:21:15 +0300

    Add a residential cleaning page

---
+++ src/pages/ResidentialCleaningPage.tsx
@@ -0,0 +1,10 @@
+const ResidentialCleaning = () => {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold">Residential Cleaning</h1>
+        <p className="mt-4">We provide top-notch house washing services.</p>
+      </div>
+    );
+  };
+  
+  export default ResidentialCleaning;
