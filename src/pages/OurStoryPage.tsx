f49ce47d0597d9e3442916636c8adb6aad0de31f
Tony Mputhia <tonymmputhia@gmail.com>
2025-03-04 21:21:03 +0300

    Add our story page

---
+++ src/pages/OurStoryPage.tsx
@@ -0,0 +1,10 @@
+const OurStory = () => {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold">Our Story</h1>
+        <p className="mt-4">We are a deep cleaning service.</p>
+      </div>
+    );
+  };
+  
+  export default OurStory;
